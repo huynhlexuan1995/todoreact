dc1bb22d3bdac8f9506c9076f0f7f23893c9eab4
huynh11a1 <huynhlx@fabbi.com.vn>
2019-01-11 10:00:57 +0700

    sort

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -22,33 +22,36 @@ class Sort extends Component {
       document.removeEventListener('click', this.closeMenu);
     });
   }
-
+  handleSort = (orderBy,orderDir)=>{
+    this.props.onClickSort(orderBy,orderDir);
+  }
   render() {
+    let {orderBy,orderDir} = this.props;
+    let strSort = orderBy+ " - " +orderDir;
     return (
       <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-          <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+          <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
               <div className="dropdown">
               <button onClick={this.showMenu} className="btn btn-defautl">Sắp xếp<span className="caret"></span></button>
               {
                 this.state.showMenu
                 ?(
                   <div className="dropdown" aria-labelledby="dropdownMenu2">
-                    <button className="btn btn-defautl dropdown-item" type="button">Name - ASC</button>
-                    <button className="btn btn-defautl dropdown-item" type="button">Name - DESC</button>
-                    <button className="btn btn-defautl dropdown-item" type="button">Level - ASC</button>
-                    <button className="btn btn-defautl dropdown-item" type="button">Level - DESC</button>
+                    <button onClick={()=>this.handleSort('Name','ASC')} className="btn btn-defautl dropdown-item" type="button">Name - ASC</button>
+                    <button onClick={()=>this.handleSort('Name','DESC')} className="btn btn-defautl dropdown-item" type="button">Name - DESC</button>
+                    <button onClick={()=>this.handleSort('Level','ASC')} className="btn btn-defautl dropdown-item" type="button">Level - ASC</button>
+                    <button onClick={()=>this.handleSort('Level','DESC')} className="btn btn-defautl dropdown-item" type="button">Level - DESC</button>
                   </div>
                 )
                 : (
                     null
                   )
               }
-          
               </div>          
           </div>           
-          <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+          <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
             <div className="sort-name">
-            <span className="label label-success">NAME - ASC</span>
+            <span className="label label-success">{ strSort }</span>
           </div> 
           </div>
       </div>
